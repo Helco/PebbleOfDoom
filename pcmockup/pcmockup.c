715f0235f1b0f4158bf9ffb4458fc9026cc8ce03
Helco <hermann.noll@hotmail.com>
2018-10-17 15:38:45 +0200

    pcmockup: Keep window alive

--- pcmockup/pcmockup.c
+++ pcmockup/pcmockup.c
@@ -3,9 +3,9 @@
 #include "pcmockup.h"
 #include "../renderer/renderer.h"
 
-
-static const int START_WINDOW_WIDTH = RENDERER_WIDTH * 4;
-static const int START_WINDOW_HEIGHT = RENDERER_HEIGHT * 4;
+static const int START_WINDOW_WIDTH = RENDERER_WIDTH * 2;
+static const int START_WINDOW_HEIGHT = RENDERER_HEIGHT * 2;
+static const int MAX_FRAMERATE = 60;
 
 #undef main
 int main(int argc, char* argv[])
@@ -25,11 +25,37 @@ int main(int argc, char* argv[])
     if (renderer == NULL)
         return -1;
         
-    // Look carefully for I will render this only once
     GColor* framebuffer = pebbleWindow_getPebbleFramebuffer(pebbleWindow);
     renderer_render(renderer, framebuffer);
-    pebbleWindow_update(pebbleWindow);
 
+    int isRunning = 1;
+    while (isRunning)
+    {
+        uint32_t frameStart = SDL_GetTicks();
+
+        pebbleWindow_update(pebbleWindow);
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                isRunning = 0;
+            else if (event.type == SDL_KEYDOWN)
+            {
+                switch(event.key.keysym.sym)
+                {
+                    case (SDLK_ESCAPE): { isRunning = 0; }break;
+                }
+            }
+        }
+
+        uint32_t frameEnd = SDL_GetTicks();
+        int delay = (1000 / MAX_FRAMERATE) - (int)(frameEnd - frameStart);
+        if (delay > 0)
+            SDL_Delay(delay);
+    }
+
+    renderer_free(renderer);
     pebbleWindow_free(pebbleWindow);
     SDL_Quit();
     return 0;
