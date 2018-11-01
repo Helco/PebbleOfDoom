017fd81cec5c8067be0f2d66d77b7ac41ae12b69
Helco <hermann.noll@hotmail.com>
2018-11-01 19:42:57 +0100

    test: fix real constant two not being initalized in MSVC

--- test/fixtures.h
+++ test/fixtures.h
@@ -8,12 +8,12 @@ extern "C" {
 class MathFixture : public testing::Test {
 protected:
     const real_t minus_one = real_from_int(-1);
+    const real_t two = real_from_int(2);
     const real_t positive = real_div(real_one, real_from_int(42));
     const real_t negative = real_div(real_from_int(-1), real_from_int(56));
     const real_t one_half = real_div(real_one, two);
     const real_t one_third = real_div(real_one, real_from_int(3));
     const real_t two_thirds = real_div(real_from_int(2), real_from_int(3));
-    const real_t two = real_from_int(2);
     const real_t three = real_from_int(3);
     const real_t four = real_from_int(4);
     const real_t halfpi = real_div(real_pi, real_from_int(2));
