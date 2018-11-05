644b0f32be3f7069715412c6a187eb074c769bc4
Helco <hermann.noll@hotmail.com>
2018-11-05 17:05:53 +0100

    tests: use real_from_float in fixtures

--- test/fixtures.h
+++ test/fixtures.h
@@ -9,11 +9,11 @@ class MathFixture : public testing::Test {
 protected:
     const real_t minus_one = real_from_int(-1);
     const real_t two = real_from_int(2);
-    const real_t positive = real_div(real_one, real_from_int(42));
-    const real_t negative = real_div(real_from_int(-1), real_from_int(56));
-    const real_t one_half = real_div(real_one, two);
-    const real_t one_third = real_div(real_one, real_from_int(3));
-    const real_t two_thirds = real_div(real_from_int(2), real_from_int(3));
+    const real_t positive = real_from_float(1 / 42.0f);
+    const real_t negative = real_from_float(-1 / 56.0f);
+    const real_t one_half = real_from_float(0.5f);
+    const real_t one_third = real_from_float(1 / 3.0f);
+    const real_t two_thirds = real_from_float(2 / 3.0f);
     const real_t three = real_from_int(3);
     const real_t four = real_from_int(4);
     const real_t halfpi = real_div(real_pi, real_from_int(2));
