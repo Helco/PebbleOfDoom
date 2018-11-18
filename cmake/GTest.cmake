0c39d88a582d3320a617ae0ac7eee4382d4f9778
Helco <hermann.noll@hotmail.com>
2018-11-18 11:15:34 +0100

    cmake: extract dependency setup code

---
+++ cmake/GTest.cmake
@@ -0,0 +1,41 @@
+# Configures googletest for use in PebbleOfDoom
+
+configure_file(external/GTest.CMakeLists.txt.in googletest-download/CMakeLists.txt)
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download )
+if(result)
+  message(FATAL_ERROR "CMake step for googletest failed: ${result}")
+endif()
+execute_process(COMMAND ${CMAKE_COMMAND} --build .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download )
+if(result)
+  message(FATAL_ERROR "Build step for googletest failed: ${result}")
+endif()
+
+# Prevent overriding the parent project's compiler/linker
+# settings on Windows
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+# Add googletest directly to our build. This defines
+# the gtest and gtest_main targets.
+add_subdirectory(${CMAKE_BINARY_DIR}/googletest-src
+                 ${CMAKE_BINARY_DIR}/googletest-build
+                 EXCLUDE_FROM_ALL)
+
+# The gtest/gtest_main targets carry header search path
+# dependencies automatically when using CMake 2.8.11 or
+# later. Otherwise we have to add them here ourselves.
+if (CMAKE_VERSION VERSION_LESS 2.8.11)
+  include_directories("${gtest_SOURCE_DIR}/include")
+endif()
+
+# To add googletests in the best way regarding cmake version
+function(add_googletest TARGET PREFIX)
+    if (CMAKE_VERSION VERSION_LESS "3.10.0")
+        gtest_add_tests(TARGET ${TARGET} TEST_PREFIX ${PREFIX})
+    else()
+        gtest_discover_tests(${TARGET} TEST_PREFIX ${PREFIX})
+    endif()
+endfunction(add_googletest)
---
+++ cmake/stb.cmake
@@ -0,0 +1,19 @@
+# Configures stb for use in PebbleOfDoom
+
+set(STB_DIR "${CMAKE_SOURCE_DIR}/external/stb")
+if (NOT EXISTS "${STB_DIR}/stb_image.h")
+    message(FATAL_ERROR "Could not find stb_image, did you clone the submodules?")
+endif()
+
+set(sources_stb
+    pcmockup/stb_impl.c
+    ${STB_DIR}/stb_image.h
+    ${STB_DIR}/stb_image_write.h
+)
+assign_source_group(${sources_stb})
+
+add_library(stb ${sources_stb})
+target_include_directories(stb PUBLIC ${STB_DIR})
+
+set(STB_LIBRARY "stb")
+set(STB_INCLUDE_DIR ${STB_DIR})
