44957a0f09868aad7096438e9053eba1ab733153
Helco <hermann.noll@hotmail.com>
2018-11-25 17:01:04 +0100

    travis: try bash local execution

--- travis/windows_before_script.sh
+++ travis/windows_before_script.sh
@@ -11,7 +11,7 @@ wget http://libsdl.org/release/SDL2-devel-2.0.9-VC.zip
 
 wget https://marketplace.visualstudio.com/_apis/public/gallery/publishers/LLVMExtensions/vsextensions/llvm-toolchain/latest/vspackage
 wget https://github.com/Microsoft/vsixbootstrapper/releases/download/1.0.31/VSIXBootstrapper.exe
-VSIXBootstrapper.exe /q llvm.vsix
+./VSIXBootstrapper.exe /q llvm.vsix
 
 export POD_CMAKE_ARGS="${POD_CMAKE_ARGS} -DSDL2_PATH=`cmd //c cd`\\SDL2-2.0.9 -T llvm"
 
