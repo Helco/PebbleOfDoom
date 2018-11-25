a654394e95922a5bfeef8b717761afe2eda6301f
Helco <hermann.noll@hotmail.com>
2018-11-25 11:05:32 +0100

    pebble: Add texture management

---
+++ pebbleapp/textureresources.h
@@ -0,0 +1,11 @@
+#ifndef TEXTURERESOURCES_H
+#define TEXTURERESOURCES_H
+#include <pebble.h>
+#include "../renderer/texture.h"
+
+#define MAX_TEXTURES 32
+
+TextureId loadTextureFromResource(uint32_t resourceId);
+void freeTextures();
+
+#endif
