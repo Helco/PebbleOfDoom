4afd34349c644ae7446d5cda70d34d1986cbb8b0
Helco <hermann.noll@hotmail.com>
2018-11-28 13:11:57 +0100

    renderer: fix logical merge error

--- renderer/renderer.c
+++ renderer/renderer.c
@@ -48,11 +48,6 @@ void renderer_setTextureManager(Renderer* me, TextureManagerHandle handle)
     me->textureManager = handle;
 }
 
-void renderer_setTextureManager(Renderer* me, TextureManagerHandle handle)
-{
-    me->textureManager = handle;
-}
-
 static xz_t myxz_rotate(xz_t a, real_t angleInRad)
 {
     real_t s = real_sin(angleInRad);
