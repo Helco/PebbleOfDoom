01b1f6fc0604624619f94e8c6a9164de7e27435f
Helco <hermann.noll@hotmail.com>
2019-05-06 15:43:03 +0200

    Level: Initial lobby

--- tools/level_tool.js
+++ tools/level_tool.js
@@ -2,49 +2,231 @@
 // LEVEL DATA
 //
 
-const XZ_FACTOR = 50;
+const XZ_FACTOR = 20;
 
-const vertices = [
+function createIncremental(arr) {
+    for (var i = 1; i < arr.length; i++) {
+        arr[i][0] += arr[i-1][0];
+        arr[i][1] += arr[i-1][1];
+    }
+    return arr;
+}
+
+const vertices = createIncremental([
     [0, 0],
-    [1, 0],
-    [0, 1],
-    [1, 1]
-];
+    [0, 2],
+    [0, 6],
+    [-12, 0],
+    [0, 12],
+    [14, 0],
+    [4, -4],
+    [16, 0],
+    [4, 0],
+    [4, 4],
+    [10, 0],
+    [0, -12],
+    [-10, 0],
+    [-6, -8],
+    [-16, 0],
+    [0, 2],
+    [-2, 0],
+    [0, 2],
+    [2, 0],
+    [0, 6],
+    [-2, 0],
+    [0, 2],
+    [2, 0],
+    [12, -0.5],
+
+    // at 23, first stair
+    [4, -3],
+    [-3.5, 3.5],
+    [4, -3],
+    [-3.5, 3.5],
+    [4, -3],
+    [-3.5, 3.5],
+    [4, -3],
+    [-3.5, 3.5],
+    [4, -3],
+
+    [-4, 3.5],
+    [4, 0],
+    [-4, 0.5],
+    [4, 0],
+    [-4, 0.5],
+    [4, 0],
+    [-4, 0.5],
+    [4, 0]
+]);
 
 const textures = {
     wall: "wall"
 };
 
+const STEP = 7;
+const defaultHeight = [0, 120];
+const defaultFloor = [1,1,1];
+const defaultCeil = [2, 2, 2];
 const sectors = {
-    left: {
-        h: [0, 30],
-        floor: [0, 0, 0],
-        ceil: [2, 2, 2],
+    a: {
         walls: [
-            [2, 0, textures.wall],
-            [1, textures.wall]
+            [13, 14, textures.wall]
         ]
     },
-    right: {
-        h: [15, 45],
-        floor: [1, 1, 1],
-        ceil: [2, 0, 0],
+    b: {
         walls: [
-            [1, 3, textures.wall],
-            [2, textures.wall]
+            [15, 18, textures.wall],
+            [19, 22, textures.wall],
+            [33, 31, textures.wall],
+            [31, 23, textures.wall]
+        ]
+    },
+    c: {
+        walls: [
+            [6, 7, textures.wall],
+            [7, 33, textures.wall],
+            [22, 21, textures.wall]
+        ]
+    },
+    d: {
+        walls: [
+            [14, 0, textures.wall],
+            [16, 15, textures.wall]
+        ]
+    },
+    e: {
+        walls: [
+            [0, 2, textures.wall],
+            [21, 20, textures.wall],
+            [17, 16, textures.wall],
+        ]
+    },
+    er: {
+        walls: [
+            [20, 19, textures.wall],
+            [18, 17, textures.wall]
+        ]
+    },
+    f: {
+        walls: [
+            [24, 32, textures.wall],
+            [12, 13, textures.wall]
+        ]
+    },
+    g: {
+        walls: [
+            [2, 3, textures.wall],
+            [3, 4, textures.wall],
+            [4, 5, textures.wall],
+            [5, 6, textures.wall]
+        ]
+    },
+    h: {
+        walls: [
+            [32, 8, textures.wall],
+            [8, 9, textures.wall],
+            [9, 10, textures.wall],
+            [10, 11, textures.wall],
+            [11, 12, textures.wall]
+        ]
+    },
+
+    // Start of stairs
+    i: {
+        h: [STEP * 1, 120],
+        walls: [
+            [23, 25, textures.wall],
+            [26, 24, textures.wall]
+        ]
+    },
+    j: {
+        h: [STEP * 2, 120],
+        walls: [
+            [25, 27, textures.wall],
+            [28, 26, textures.wall],
+        ]
+    },
+    k: {
+        h: [STEP * 3, 120],
+        walls: [
+            [27, 29, textures.wall],
+            [30, 28, textures.wall],
+        ]
+    },
+    l: {
+        h: [STEP * 4, 120],
+        walls: [
+            [29, 31, textures.wall],
+            [32, 30, textures.wall],
+        ]
+    },
+    m: {
+        h: [STEP * 5, 120],
+        walls: [
+            [31, 33, textures.wall],
+            [34, 32, textures.wall],
+        ]
+    },
+    n: {
+        h: [STEP * 6, 120],
+        walls: [
+            [33, 35, textures.wall],
+            [36, 34, textures.wall],
+        ]
+    },
+    o: {
+        h: [STEP * 7, 120],
+        walls: [
+            [35, 37, textures.wall],
+            [38, 36, textures.wall],
+        ]
+    },
+    p: {
+        h: [STEP * 8, 120],
+        walls: [
+            [37, 39, textures.wall],
+            [40, 38, textures.wall],
+        ]
+    },
+    q: {
+        h: [STEP * 9, 120],
+        walls: [
+            [39, 7, textures.wall],
+            [7, 8, textures.wall],
+            [8, 40, textures.wall],
         ]
     }
 };
 
 const portals = [
-    [1, 2, sectors.left, sectors.right]
+    [6, 21, sectors.g, sectors.c],
+    [21, 2, sectors.g, sectors.e],
+    [20, 17, sectors.e, sectors.er],
+    [16, 0, sectors.e, sectors.d],
+    [33, 22, sectors.c, sectors.b],
+    [18, 19, sectors.b, sectors.er],
+    [14, 15, sectors.b, sectors.d],
+    [23, 14, sectors.b, sectors.a],
+    [24, 13, sectors.a, sectors.f],
+    [32, 12, sectors.f, sectors.h],
+
+    // Start of stairs
+    [23, 24, sectors.a, sectors.i],
+    [25, 26, sectors.i, sectors.j],
+    [27, 28, sectors.j, sectors.k],
+    [29, 30, sectors.k, sectors.l],
+    [31, 32, sectors.l, sectors.m],
+    [33, 34, sectors.m, sectors.n],
+    [35, 36, sectors.n, sectors.o],
+    [37, 38, sectors.o, sectors.p],
+    [39, 40, sectors.p, sectors.q],
 ];
 
 const player = {
-    sector: 0,
-    startPos: [ 0.1, 0.1 ],
-    angle: 34,
-    height: 10
+    sector: sectors.a,
+    startPos: [ 20, 4],
+    angle: 0,
+    height: 5
 };
 
 //
@@ -121,9 +303,9 @@ let sectorObjects = [];
         let sectorObject = {
             index: sector.index,
             name: sectorName,
-            floor: checkColor(sector.floor),
-            ceil: checkColor(sector.ceil),
-            height: checkVector(sector.h),
+            floor: checkColor(sector.floor || defaultFloor.slice(0)),
+            ceil: checkColor(sector.ceil || defaultCeil.slice(0)),
+            height: checkVector(sector.h || defaultHeight.slice(0)),
             walls: []
         };
         for (let i = 0; i < sector.walls.length; i++)
@@ -136,8 +318,8 @@ let sectorObjects = [];
             if (arr.length < 5)
                 arr.push([0, 0], [1, 1]);
             sectorObject.walls[i] = {
-                from: checkVertexIndex(arr[0]),
-                to: checkVertexIndex(arr[1]),
+                from: checkVertexIndex(arr[1]),
+                to: checkVertexIndex(arr[0]),
                 textureI: getTextureIndex(arr[2]),
                 texStart: checkVector(arr[3]),
                 texEnd: checkVector(arr[4]),
@@ -160,16 +342,16 @@ let sectorObjects = [];
 
         const fromI = portal[2].index, toI = portal[3].index;
         sectorObjects[fromI].walls.push({
-            from: checkVertexIndex(portal[0]),
-            to: checkVertexIndex(portal[1]),
+            from: checkVertexIndex(portal[1]),
+            to: checkVertexIndex(portal[0]),
             textureI: getTextureIndex(portal[4]),
             texStart: checkVector(portal[5]),
             texEnd: checkVector(portal[6]),
             portalTo: toI
         });
         sectorObjects[toI].walls.push({
-            from: checkVertexIndex(portal[1]),
-            to: checkVertexIndex(portal[0]),
+            from: checkVertexIndex(portal[0]),
+            to: checkVertexIndex(portal[1]),
             textureI: getTextureIndex(portal[4]),
             texStart: checkVector(portal[5]),
             texEnd: checkVector(portal[6]),
@@ -227,7 +409,15 @@ function printColor(color) {
         (color[2] * F) + ")";
 }
 
-let OUTPUT = "";
+let OUTPUT =
+`#include <pebble.h>
+#include "platform.h"
+#include "level.h"
+
+Level *level_load(int levelId)
+{
+    UNUSED(levelId);
+`;
 
 // Output walls_template
 OUTPUT += "\tWall walls_template[] = {\n";
@@ -265,7 +455,7 @@ OUTPUT +=
     `\tLevel level_template = {\n` +
     `\t\t.sectorCount = ${sectorObjects.length},\n` +
     `\t\t.playerStart = {\n` +
-    `\t\t\t.sector = ${player.sector},\n` +
+    `\t\t\t.sector = ${player.sector.index},\n` +
     `\t\t\t.position = ${printXZ(checkVector(player.startPos))},\n` +
     `\t\t\t.angle = real_degToRad(${printReal(player.angle)}),\n` +
     `\t\t\t.height = ${printReal(player.height)}\n` +
@@ -294,5 +484,17 @@ for (let sectorObject of sectorObjects)
     OUTPUT += `\tsectors[${sectorObject.index}].walls = walls + ${wallOffset};\n`;
     wallOffset += sectorObject.wallChain.length;
 }
+
+OUTPUT +=
+`return level;
+}
+
+void level_free(Level *me)
+{
+    if (me == NULL)
+        return;
+    free(me);
+}
+`;
 
 require("fs").writeFileSync("levelTempl.c", OUTPUT);
